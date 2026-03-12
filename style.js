815ab876d7cb217c873421b3d09de6db4ed0bbf8
SoftwareDev-EzinneO <esthyonuoha10@gmail.com>
2026-03-12 08:24:21 +0100

    first commit

---
+++ style.js
@@ -0,0 +1,8 @@
+let year = prompt ("Welcome to netflix, please enter your age");
+if (year < 18) {
+    alert ("sorry you are not old enough to watch this content");
+} else {
+    alert ("CORRECT, you are old enough to watch this content");
+    alert ("WELCOME TO NETFLIX");
+    alert ("NETFLIX IS THE BEST STREAMING SERVICE IN THE WORLD");
+}
